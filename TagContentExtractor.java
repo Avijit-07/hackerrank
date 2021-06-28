a3bc2f221d9b515c0a2ca944cb1974a6413e97d4
Avijit Maiti <avijit.haldia@gmail.com>
2021-06-28 07:14:08 +0530

    Create TagContentExtractor.java

---
+++ TagContentExtractor.java
@@ -0,0 +1,25 @@
+public class TagContentExtractor {
+
+    public static void main(String[] args) {
+        String s = "<h1>Nayeem loves counseling</h1>";
+        tagExtractor(s);
+    }
+
+    private static void tagExtractor(String line){
+
+        boolean matchFound = false;
+
+        Pattern r = Pattern.compile("<(.+)>([^<]+)</\\1>");
+        Matcher matcher = r.matcher(line);
+
+        while(matcher.find()){
+            System.out.println(matcher.group(2));
+            matchFound = true;
+        }
+
+        if(!matchFound){
+            System.out.println("None");
+        }
+
+    }
+}

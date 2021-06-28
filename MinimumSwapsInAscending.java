2ee22be8211bdcee1bf5f5e2331e92010fd2df5c
Avijit Maiti <avijit.haldia@gmail.com>
2021-06-28 06:21:09 +0530

    Create MinimumSwapsInAscending.java

---
+++ MinimumSwapsInAscending.java
@@ -0,0 +1,28 @@
+public class MinimumSwapsInAscending {
+
+    static int minimumSwaps(int[] arr) {
+        int length = arr.length;
+        int i = arr.length;
+        int k = 0;
+        int temp;
+        int count = 0;
+        while (k < length && i - k >=0) {
+            if (arr[k] != i - k) {
+                temp = arr[k];
+                arr[k] = arr[temp + k];
+                arr[temp + k] = temp;
+                count++;
+            } else {
+                i--;
+                k++;
+                continue;
+            }
+            k++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minimumSwaps(new int[] { 3, 2, 1, 4 }));
+    }
+}

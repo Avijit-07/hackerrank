9f2b9090e8363f69e97445dd88015a183e858b57
avijit_maiti <avijit.haldia@gmail.com>
2025-07-21 19:45:07 +0100

    PrimeChecker - brain dump

---
+++ PrimeChecker.java
@@ -0,0 +1,46 @@
+package hackerrank;
+
+/**
+ * You are given a class Solution and its main method in the editor.
+ * Your task is to create a class Prime.
+ * The class Prime should contain a single method checkPrime.
+ * The locked code in the editor will call the checkPrime method with one or more integer arguments.
+ * You should write the checkPrime method in such a way that the code prints only the prime numbers.
+
+
+ Input Format
+ There are only five lines of input, each containing one integer.
+
+ Output Format
+ There will be only four lines of output.
+ Each line contains only prime numbers depending upon the parameters passed to checkPrime in the main method of the class Solution.
+ In case there is no prime number, then a blank line should be printed.
+
+ <a href="https://www.hackerrank.com/challenges/prime-checker/problem?isFullScreen=true">Prime Checker HR</a>
+
+ */
+
+/**
+ * prime number ==> 2, 3, 5, 7
+ * non prime numbers ==>1, 4, 6, 8, 9, 10, 12
+ * =======================================
+    6 % 1 == 0
+    6 % 6 == 0
+    6 % 2 == 0 ---> Non prime
+
+    9 % 1 == 0
+    9 % 9 == 0
+    9 % 2 != 0
+    9 % 3 == 0 ----> non prime
+
+    5 % 1 == 0
+    5 % 5 == 0
+    5 % 2 != 0
+    5 % 3 != 0
+    5 % 4 != 0
+
+*/
+
+
+public class PrimeChecker {
+}
